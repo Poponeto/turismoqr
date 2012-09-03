ea3ed5043200fbdc3f24bf54872307951230b7ef
fede_takin <fede_takin@hotmail.com>
2012-09-03 18:38:40 +0000

    Agregando logica de negocio. manejadorEstado, cambiando manejadorIdioma y ServicioConsultaPuntoInteres

--- TurismoQR.ServiciosWeb/src/java/ServicioConsultaPuntoInteres/ServicioConsultaPuntoInteres.java
+++ TurismoQR.ServiciosWeb/src/java/ServicioConsultaPuntoInteres/ServicioConsultaPuntoInteres.java
@@ -5,12 +5,14 @@
 package ServicioConsultaPuntoInteres;
 
 import TurismoQR.AccesoDatos.IAccesoDatos;
+import TurismoQR.ObjetosNegocio.Informacion.Idioma;
 import TurismoQR.ObjetosNegocio.Informacion.Imagen;
 import TurismoQR.ObjetosNegocio.Informacion.InformacionEnIdioma;
 import TurismoQR.ObjetosNegocio.Punto.Punto;
 import TurismoQR.ObjetosTransmisionDatos.DTOImagen;
 import TurismoQR.ObjetosTransmisionDatos.DTOInformacionEnIdioma;
 import TurismoQR.ObjetosTransmisionDatos.DTOPunto;
+import TurismoQR.Punto.ManejadorEstados.ManejadorEstados;
 import TurismoQR.Punto.ManejadorIdiomas.ManejadorIdiomas;
 import TurismoQR.Traductores.ITraductor;
 import java.util.ArrayList;
@@ -34,27 +36,35 @@ public class ServicioConsultaPuntoInteres
     private ITraductor traductor;
     @Resource
     private ManejadorIdiomas manejadorIdioma;
-
+    @Resource
+    private ManejadorEstados manejadorEstado;
     /**
      * Web service operation
      */
     @WebMethod(operationName = "ConsultarPuntoInteres")
     public DTOPunto ConsultarPuntoInteres(@WebParam(name = "idPuntoInteres")
-            final String idPuntoInteres,@WebParam(name = "idioma") final String idioma)
+            final String idPuntoInteres,@WebParam(name = "nombreIdioma") final String nombreIdioma)
     {
         Punto punto = accesoDatos.BuscarObjeto(idPuntoInteres);
-        InformacionEnIdioma informacionPunto = manejadorIdioma.seleccionarInformacionDePuntoEnIdioma(punto);
 
-        DTOPunto dtoPunto = (DTOPunto) traductor.traducir(punto);
-        DTOInformacionEnIdioma dtoInformacion = (DTOInformacionEnIdioma) traductor.traducir(informacionPunto);
+        if (punto != null && manejadorEstado.esEstadoValidoConsulta(punto.getEstado()))
+        {
+            Idioma idioma = manejadorIdioma.obtenerIdioma(nombreIdioma);
+            InformacionEnIdioma informacionPunto = manejadorIdioma.seleccionarInformacionDePuntoEnIdioma(punto, idioma);
 
-        dtoPunto.setImagenes(crearDTOImagenes(punto.getImagenes(), idioma));
-        dtoPunto.setInformacion(dtoInformacion);
+            DTOPunto dtoPunto = (DTOPunto) traductor.traducir(punto);
+            DTOInformacionEnIdioma dtoInformacion = (DTOInformacionEnIdioma) traductor.traducir(informacionPunto);
 
-        return dtoPunto;
+            dtoPunto.setImagenes(crearDTOImagenes(punto.getImagenes(), idioma));
+            dtoPunto.setInformacion(dtoInformacion);
+
+            return dtoPunto;
+        }
+
+        return null;
     }
 
-    public Collection<DTOImagen> crearDTOImagenes(Collection<Imagen> imagenes, final String idioma)
+    public Collection<DTOImagen> crearDTOImagenes(Collection<Imagen> imagenes, final Idioma idioma)
     {
         Collection<DTOImagen> dtoImagenes = new ArrayList<DTOImagen>();
 
@@ -66,9 +76,9 @@ public class ServicioConsultaPuntoInteres
         return dtoImagenes;
     }
 
-    public DTOImagen crearDTOImagen(Imagen imagen, final String idioma)
+    public DTOImagen crearDTOImagen(Imagen imagen, final Idioma idioma)
     {
-        InformacionEnIdioma informacionImagen = manejadorIdioma.seleccionarInformacionDeImagenEnIdioma(imagen);
+        InformacionEnIdioma informacionImagen = manejadorIdioma.seleccionarInformacionDeImagenEnIdioma(imagen, idioma);
 
         DTOImagen dtoImagen = (DTOImagen) traductor.traducir(imagen);
         DTOInformacionEnIdioma dtoInformacion = (DTOInformacionEnIdioma) traductor.traducir(informacionImagen);

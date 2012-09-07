92168b169e52f9f8103a0ae2322882193f305064
fede_takin <fede_takin@hotmail.com>
2012-09-07 20:09:12 +0000

    Configuraciones de Spring

---
+++ TurismoQR.Negocio/src/TurismoQR/Usuario/ManejadorLogin/ManejadorLogin.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package TurismoQR.Usuario.ManejadorLogin;
+
+
+
+import TurismoQR.AccesoDatos.AccesoDatosUsuario;
+import TurismoQR.ObjetosNegocio.Usuarios.Usuario;
+import TurismoQR.ObjetosTransmisionDatos.DTOUsuario;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Federico
+ */
+
+@Service
+public class ManejadorLogin {
+
+    @Resource
+    private AccesoDatosUsuario accesoDatosUsuario;
+
+    public Boolean esUsuarioDelSistema(DTOUsuario dtoUsuario)
+    {
+        try
+        {
+            Usuario usuario = accesoDatosUsuario.buscarUsuario(dtoUsuario.getNombreUsuario(), dtoUsuario.getContraseña());
+
+            if (usuario != null)
+            {
+                return true;
+            }
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+
+        return false;
+    }
+}

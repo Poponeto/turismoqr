73cc9c0ebc60cb74122513d70c3699371a2ddf9c
tacchinifederico <tacchinifederico@gmail.com>
2012-09-19 18:41:14 +0000

    Issue 5: Crear la estructura de la página. http://code.google.com/p/turismoqr/issues/detail?id=5

---
+++ TurismoQR.Web/src/java/Controladores/BuscarPuntoController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author ftacchini
+ */
+
+@Controller("buscarPunto")
+public class BuscarPuntoController {
+
+    @RequestMapping("/paginaBuscarPunto.htm")
+    public String redirigir()
+    {
+        return "Punto/BuscarPunto";
+    }
+}
--- TurismoQR.Web/src/java/Controladores/LoginController.java
+++ TurismoQR.Web/src/java/Controladores/LoginController.java
@@ -7,7 +7,8 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /*
  * To change this template, choose Tools | Templates
@@ -19,7 +20,7 @@ import org.springframework.stereotype.Service;
  * @author Federico
  */
 
-@Service("loginController")
+@Controller("login")
 public class LoginController implements UserDetailsService {
 
     
@@ -35,6 +36,12 @@ public class LoginController implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException, DataAccessException
     {
         return servicioUsuario.cargarUsuario(username);
+    }
+
+    @RequestMapping("/ingresarComoTurista.htm")
+    public String ingresarComoTurista()
+    {
+        return "/buscarPunto/paginaBuscarPunto.htm";
     }
 
  

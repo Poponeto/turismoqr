326e33e7941bab3e6a837c3a7ecdd538a5884104
tacchinifederico <tacchinifederico@gmail.com>
2012-10-11 16:26:27 +0000

    Issue 1: Consultar punto interes http://code.google.com/p/turismoqr/issues/detail?id=1

--- TurismoQR.Web/src/java/Controladores/BuscarPuntoController.java
+++ TurismoQR.Web/src/java/Controladores/BuscarPuntoController.java
@@ -6,7 +6,9 @@
 package Controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -17,8 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/buscarPunto")
 public class BuscarPuntoController {
 
-    @RequestMapping("/paginaBuscarPunto.htm")
-    public String redirigir()
+    @RequestMapping(value = "/paginaBuscarPunto.htm",method = RequestMethod.GET)
+    public String redirigir(ModelMap model)
     {
         return "Punto/BuscarPunto";
     }

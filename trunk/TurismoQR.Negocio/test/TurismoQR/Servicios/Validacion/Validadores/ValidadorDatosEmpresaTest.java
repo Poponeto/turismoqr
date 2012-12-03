e57b8b0adcda7ed77cd3ebc05fedadeee64e1e4c
rodri.dpf <rodri.dpf@5bb9eb9b-eec0-e6f5-1d77-51e469810d75>
2012-12-03 03:37:03 +0000

    Agrego ValidadorDatosEmpresaTest, Verificar algunas validaciones

---
+++ trunk/TurismoQR.Negocio/test/TurismoQR/Servicios/Validacion/Validadores/ValidadorDatosEmpresaTest.java
@@ -0,0 +1,135 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TurismoQR.Servicios.Validacion.Validadores;
+
+import TurismoQR.AccesoDatos.IAccesoDatos;
+import TurismoQR.ObjetosTransmisionDatos.*;
+import TurismoQR.Servicios.Validacion.Errores;
+import TurismoQR.Servicios.Validacion.IServicioValidacionDatos;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author Rodrius
+ */
+public class ValidadorDatosEmpresaTest {
+
+    public ValidadorDatosEmpresaTest() {
+    }
+
+    @Test
+    public void testSoportaObjeto() {
+
+        DTOContacto dtoContacto = mock(DTOContacto.class);
+        DTOCliente dtoCliente = mock(DTOCliente.class);
+        DTOEmpresa dtoEmpresa = mock(DTOEmpresa.class);
+        DTOContactoEmpresa dtoContactoEmpresa = mock(DTOContactoEmpresa.class);
+
+        IAccesoDatos accesoDatos = mock(IAccesoDatos.class);
+        IServicioValidacionDatos validacionDatos = mock(IServicioValidacionDatos.class);
+
+        ValidadorDatosEmpresa validadorDatosEmpresa = new ValidadorDatosEmpresa(accesoDatos, validacionDatos);
+        boolean resultadoDTOContacto = validadorDatosEmpresa.soportaObjeto(dtoContacto);
+        boolean resultadoDTOCliente = validadorDatosEmpresa.soportaObjeto(dtoCliente);
+        boolean resultadoDTOEmpresa = validadorDatosEmpresa.soportaObjeto(dtoEmpresa);
+        boolean resultadoDTOContactoEmpresa = validadorDatosEmpresa.soportaObjeto(dtoContactoEmpresa);
+
+        assertFalse(resultadoDTOContacto);
+        assertFalse(resultadoDTOCliente);
+        assertTrue(resultadoDTOEmpresa);
+        assertFalse(resultadoDTOContactoEmpresa);
+    }
+
+    /**
+     * Test of validar method, of class ValidadorDatosPersona.
+     */
+    @Test
+    public void testValidarExito() {
+        Errores errores1 = mock(Errores.class);
+        Errores errores2 = mock(Errores.class);
+        Errores errores3 = mock(Errores.class);
+        DTOEmpresa dtoEmpresa = mock(DTOEmpresa.class);
+        DTORubro dtoRubro = mock(DTORubro.class);
+        IAccesoDatos accesoDatos = mock(IAccesoDatos.class);
+        IServicioValidacionDatos validacionDatos = mock(IServicioValidacionDatos.class);
+
+
+
+
+
+        when(dtoEmpresa.getRazonSocial()).thenReturn("S.A");
+        when(dtoEmpresa.getCuit()).thenReturn("20-33953934-1");
+//        when(dtoEmpresa.getRubro().getNombreRubro()).thenReturn("Comercial");
+        //Esta fallando este When.. tira error. El resto funciona a la perfeccion
+
+        ValidadorDatosEmpresa validadorDatosEmpresa = new ValidadorDatosEmpresa(accesoDatos, validacionDatos);
+        validadorDatosEmpresa.validar(dtoEmpresa, errores1);
+        validadorDatosEmpresa.validar(dtoEmpresa, errores2);
+        //    validadorDatosEmpresa.validar(dtoEmpresa, errores3);
+
+
+        verify(errores1, times(0)).agregarError(eq("razonSocial"), eq("Debe especificar Razon Social de la empresa."));
+        verify(errores2, times(0)).agregarError(eq("cuit"), eq("Debe especificar un numero de CUIT."));
+        verify(errores2, times(0)).agregarError(eq("cuit"), eq("El CUIT no tiene un formato valido. El formato es nn-nnnnnnnn-n"));
+        verify(errores2, times(0)).agregarError(eq("cuit"), eq("Ya existe una empresa con ese numeor de CUIT cargada en el sistema."));
+        // verify(errores3, times(0)).agregarError(eq("rubro"), eq("Debe especificar un rubro para la empresa."));
+
+        // verify(errores3, times(0)).agregarError(eq("rubro"), eq("El rubro especificado no existe."));
+        // verify(errores2, times(0)).agregarError(eq("contactos"), eq(""));
+
+
+
+    }
+
+    @Test
+    public void testValidarFracaso() {
+        Errores errores1 = mock(Errores.class);
+        Errores errores2 = mock(Errores.class);
+        Errores errores3 = mock(Errores.class);
+        Errores errores4 = mock(Errores.class);
+        Errores errores5 = mock(Errores.class);
+
+        DTOEmpresa dtoEmpresa = mock(DTOEmpresa.class);
+        DTORubro dtoRubro = mock(DTORubro.class);
+
+
+        IAccesoDatos accesoDatos = mock(IAccesoDatos.class);
+        IServicioValidacionDatos validacionDatos = mock(IServicioValidacionDatos.class);
+
+
+        when(dtoEmpresa.getRazonSocial()).thenReturn(null).thenReturn("Cualquiera");
+        when(dtoEmpresa.getCuit()).thenReturn(null).thenReturn("20339539341").thenReturn("20-33953934-1");
+        //  when(dtoEmpresa.getRubro().getNombreRubro()).thenReturn(null).thenReturn("Cartonero");
+        //Esta fallando este When.. tira error. El resto funciona a la perfeccion
+
+        ValidadorDatosEmpresa validadorDatosEmpresa = new ValidadorDatosEmpresa(accesoDatos, validacionDatos);
+        validadorDatosEmpresa.validar(dtoEmpresa, errores1);
+        validadorDatosEmpresa.validar(dtoEmpresa, errores2);
+       validadorDatosEmpresa.validar(dtoEmpresa, errores3);
+
+        verify(errores1, times(1)).agregarError(eq("razonSocial"), eq("Debe especificar Razon Social de la empresa."));
+        verify(errores1, times(1)).agregarError(eq("cuit"), eq("Debe especificar un numero de CUIT."));
+        verify(errores1, times(1)).agregarError(eq("rubro"), eq("Debe especificar un rubro para la empresa."));
+        
+     //   verify(errores2, times(1)).agregarError(eq("cuit"), eq("El CUIT no tiene un formato valido. El formato es nn-nnnnnnnn-n"));
+        verify(errores2, times(1)).agregarError(eq("rubro"), eq("Debe especificar un rubro para la empresa."));
+        
+        
+    //    verify(errores3, times(1)).agregarError(eq("cuit"), eq("Ya existe una empresa con ese numeor de CUIT cargada en el sistema."));
+        verify(errores3, times(1)).agregarError(eq("rubro"), eq("Debe especificar un rubro para la empresa."));
+     
+    //    verify(errores3, times(1)).agregarError(eq("rubro"), eq("El rubro especificado no existe."));
+
+        //  verify(errores2, times(1)).agregarError(eq("contactos"), eq(""));
+
+
+
+    }
+}

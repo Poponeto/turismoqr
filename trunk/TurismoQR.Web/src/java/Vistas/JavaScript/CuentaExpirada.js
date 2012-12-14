76399bda290d8c693ee2b4612790230825778406
tacchinifederico@gmail.com <tacchinifederico@gmail.com@5bb9eb9b-eec0-e6f5-1d77-51e469810d75>
2012-12-14 12:32:07 +0000

    InformacionPersonal

--- trunk/TurismoQR.Web/src/java/Vistas/JavaScript/CuentaExpirada.js
+++ trunk/TurismoQR.Web/src/java/Vistas/JavaScript/CuentaExpirada.js
@@ -14,38 +14,3 @@ function inicializarDatosCuentaExpirada(urlBase)
     });
 }
 
-function actualizarContraseña(urlBase)
-{
-    if (hayErrores())
-    {
-        alert("Hay errores, solucionelos antes de continuar.");
-    }
-    else
-    {
-        var datosCambioContraseña = obtenerDatosCambioContraseña();
-        var urlAccion = urlBase + "/administracion/usuario/cambiarContrasenia.htm";
-
-        if (probarNuevaContraseña(datosCambioContraseña))
-        {
-
-            delete datosCambioContraseña[$("#repitaPassword").attr("name")];
-
-            $.ajax({
-                url: urlAccion,
-                dataType: "json",
-                data: datosCambioContraseña,
-                type: "POST",
-                success: function(data){
-                    var urlRedirect = urlBase + "/" +data["urlRedirigir"];
-                    window.location.replace(urlRedirect);
-                },
-                error: function(jqXHR){
-                    var errores = jqXHR["responseText"];
-                    procesarErrores(jQuery.parseJSON(errores));
-                }
-            });
-        }
-        
-
-    }
-}
--- trunk/TurismoQR.Web/src/java/Vistas/JavaScript/DatosCambioContrasenia.js
+++ trunk/TurismoQR.Web/src/java/Vistas/JavaScript/DatosCambioContrasenia.js
@@ -41,3 +41,39 @@ function probarNuevaContraseña(datosCambioContraseña)
     $("#contenedorNuevoPassword").append('<p class="mensajeErrorEntrada">Las contraseñas no coinciden.</p>')
     return false;
 }
+
+function actualizarContraseña(urlBase)
+{
+    if (hayErrores())
+    {
+        alert("Hay errores, solucionelos antes de continuar.");
+    }
+    else
+    {
+        var datosCambioContraseña = obtenerDatosCambioContraseña();
+        var urlAccion = urlBase + "/administracion/usuario/cambiarContrasenia.htm";
+
+        if (probarNuevaContraseña(datosCambioContraseña))
+        {
+
+            delete datosCambioContraseña[$("#repitaPassword").attr("name")];
+
+            $.ajax({
+                url: urlAccion,
+                dataType: "json",
+                data: datosCambioContraseña,
+                type: "POST",
+                success: function(data){
+                    var urlRedirect = urlBase + "/" +data["urlRedirigir"];
+                    window.location.replace(urlRedirect);
+                },
+                error: function(jqXHR){
+                    var errores = jqXHR["responseText"];
+                    procesarErrores(jQuery.parseJSON(errores));
+                }
+            });
+        }
+
+
+    }
+}
